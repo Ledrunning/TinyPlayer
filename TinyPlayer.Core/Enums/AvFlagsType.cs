98611ae1db2f4cb68bad179fb1fcd6b6fe62abd2
Ledrunning <ledrunner@hotmail.com>
2024-10-06 20:01:16 +0200

    Added enum and service class in core

---
+++ TinyPlayer.Core/Enums/AvFlagsType.cs
@@ -0,0 +1,13 @@
+﻿namespace TinyPlayer.Core.Enums;
+
+[Flags]
+public enum AvFlagsType : uint
+{
+    Video = 1 << 0,
+    Audio = 1 << 1,
+    SubText = 1 << 2,
+
+    EnableAllFlags = Video | Audio | SubText,
+    DisableSubtitles = Video | (Audio & ~SubText),
+    DisableAudio = Video | (SubText & ~Audio)
+}
---
+++ TinyPlayer.Core/VideoPlayer.cs
@@ -0,0 +1,19 @@
+﻿using Gst;
+using GLib;
+using TinyPlayer.Core.Enums;
+using Value = GLib.Value;
+
+namespace TinyPlayer.Core;
+
+public class VideoPlayer
+{
+    private readonly Element _playbin;
+
+    private void SetFlags()
+    {
+        var flags = (uint)_playbin["flags"];
+
+        flags = (uint)(AvFlagsType.Audio | AvFlagsType.Video | AvFlagsType.SubText);
+        _playbin.SetProperty("flags", new Value(flags));
+    }
+}
