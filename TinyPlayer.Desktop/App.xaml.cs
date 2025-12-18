209267ec7454f840f758155be5dae0d1c9fb2920
Ledrunning <ledrunner@hotmail.com>
2025-12-18 21:03:32 +0100

    Data context has been added

--- TinyPlayer.Desktop/App.xaml.cs
+++ TinyPlayer.Desktop/App.xaml.cs
@@ -28,7 +28,7 @@ namespace TinyPlayer
         // https://docs.microsoft.com/dotnet/core/extensions/logging
         private static readonly IHost _host = Host
             .CreateDefaultBuilder()
-            .ConfigureAppConfiguration(c => { c.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)); })
+            .ConfigureAppConfiguration(c => { c.SetBasePath(basePath: Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)); })
             .ConfigureServices((context, services) =>
             {
 
@@ -43,7 +43,7 @@ namespace TinyPlayer
         /// </summary>
         /// <typeparam name="T">Type of the service to get.</typeparam>
         /// <returns>Instance of the service or <see langword="null"/>.</returns>
-        public static T GetService<T>()
+        public static T? GetService<T>()
             where T : class
         {
             return _host.Services.GetService(typeof(T)) as T;
